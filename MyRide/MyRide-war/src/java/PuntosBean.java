608e4bf75fc5afdf4e21720947e23096946d688b
simulacion22 <simulacion22@gmail.com>
2013-04-24 15:35:14 +0000

    modificacion en clases

--- MyRide/MyRide-war/src/java/PuntosBean.java
+++ MyRide/MyRide-war/src/java/PuntosBean.java
@@ -4,11 +4,13 @@
  */
 
 import bl.PuntosBlLocal;
+import bl.RutaBlLocal;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import modelos.Puntos;
+import modelos.Ruta;
 
 /**
  *
@@ -18,8 +20,12 @@ import modelos.Puntos;
 @SessionScoped
 public class PuntosBean {
     @EJB
+    private RutaBlLocal rutaBl;
+    @EJB
     private PuntosBlLocal puntosBl;
+    
     private List<Puntos> listaPunto;
+    private List<Ruta> listaRuta;
     private Puntos puntos;
 
     public Puntos getPuntos() {
@@ -31,19 +37,23 @@ public class PuntosBean {
     }
 
     public List<Puntos> getListaPunto() {
+        listaPunto=puntosBl.obtenerPuntos();
         return listaPunto;
     }
-
-    public void setListaPunto(List<Puntos> listaPunto) {
-        this.listaPunto = listaPunto;
-    }
     
+    public List<Ruta> getListaRuta(){
+    listaRuta=rutaBl.obtenerRuta();
+    return listaRuta;
+    }
             
     /**
      * Creates a new instance of PuntosBean
      */
     public PuntosBean() {
+        puntos=new Puntos();
     }
+    
+    
     public String crear(){
     puntosBl.registrar(puntos);
     return "ListaPuntos";
