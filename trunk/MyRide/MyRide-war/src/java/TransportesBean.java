36bfc23915de02052e4faeecee778b7a8a674c89
quiterio_nemesis@hotmail.com <quiterio_nemesis@hotmail.com@dbbf7307-0e9e-8db5-3781-085e3c10f031>
2013-05-13 16:22:23 +0000

    WAR/transporte terminado lo de FIXME, ya agrega con lo del box "Me dijo Yuri"

--- trunk/MyRide/MyRide-war/src/java/TransportesBean.java
+++ trunk/MyRide/MyRide-war/src/java/TransportesBean.java
@@ -3,28 +3,35 @@
  * and open the template in the editor.
  */
 
+import bl.RutaBlLocal;
 import bl.TransporteBlLocal;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.event.ActionEvent;
+import modelos.Ruta;
 import modelos.Transporte;
 
 /**
  *
  * @author Santiago
- */ 
+ */
 @ManagedBean
 @SessionScoped
 public class TransportesBean {
-    //-------------------Variables(propiedades)-------
 
+    //-------------------Variables(propiedades)-------
+    @EJB
+    private RutaBlLocal rutaBl;
     @EJB
     private TransporteBlLocal transporteBl;
+    
     private Transporte transporte;
     private String accion;
     private List<Transporte> lista;
+    private List<Ruta> listaRuta;
+    private int idRutaSeleccionada;
     //-------------------Constructor-------
 
     public TransportesBean() {
@@ -37,6 +44,19 @@ public class TransportesBean {
         return lista;
     }
 
+    public List<Ruta> getListaRuta() {
+        listaRuta = rutaBl.obtenerRuta();
+        return listaRuta;
+    }
+
+    public int getIdRutaSeleccionada() {
+        return idRutaSeleccionada;
+    }
+
+    public void setIdRutaSeleccionada(int idRutaSeleccionada) {
+        this.idRutaSeleccionada = idRutaSeleccionada;
+    }
+    
     public Transporte getTransporte() {
         return transporte;
     }
@@ -44,7 +64,7 @@ public class TransportesBean {
     public void setTransporte(Transporte transporte) {
         this.transporte = transporte;
     }
-    
+
     //-------------------Acciones-------
     public String procesarAccion() {
         System.out.println("accion autor>>>>" + accion);
@@ -58,8 +78,16 @@ public class TransportesBean {
         accion = "";
         return "TransportesLista";
     }
-
+    public String procesarSeleccion(){
+        transporte.setRutaid(rutaBl.getRutaPorId(idRutaSeleccionada));
+        return "TransportesConfirmar";
+    }
     //-------------------Listener-------
+    public String prepararRuta() {
+        transporte.setRutaid(rutaBl.getRutaPorId(idRutaSeleccionada));
+        return "TransportesConfirmar";
+    }
+    
     public void prepararNuevo(ActionEvent evt) {
         transporte = new Transporte();
         accion = "Nuevo";
